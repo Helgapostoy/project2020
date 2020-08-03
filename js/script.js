310a93bf619a266a95d2c3467b00ee8a10da896c
Olga Postoy <leeloo-5element@mail.ru>
2020-08-03 16:32:36 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use strict"
+
+//const result = confirm('are you here?');
+//console.log(result);
+
+//const answer = +prompt('Вам есть 18 лет?', '');
+//console.log(typeof(answer));
+
+const answers = [];
+
+answers[0] = prompt('Как ваше имя?', '');
+answers[1] = prompt('Как ваша фамилия?', '');
+answers[2] = prompt('Сколько вам лет?', '');
+
+document.write(answers);
